08cc46b70c23aaaf016f8a16ac56889a7236d9b9
brianmat@uw.edu <brianmat@uw.edu@96964130-56d7-0125-8f55-f3c4090f176b>
2013-02-14 16:20:15 +0000

    Added BaseScreen.

---
+++ trunk/src/ui/BaseScreen.java
@@ -0,0 +1,13 @@
+package ui;
+
+import javax.swing.JPanel;
+
+public class BaseScreen extends JPanel {
+    private static final long serialVersionUID = 8504668632569400294L;
+    
+    public App application;
+
+    public BaseScreen(App application) {
+        this.application = application;
+    }
+}
