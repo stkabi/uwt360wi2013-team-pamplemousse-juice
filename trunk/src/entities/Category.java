70fcb5a1881e670a64283d943b8fceb447cf8d6f
brianmat@uw.edu <brianmat@uw.edu@96964130-56d7-0125-8f55-f3c4090f176b>
2013-02-14 18:14:25 +0000

    Adding judgeIDS to category. Fixing enabled/disabled buttons.

--- trunk/src/entities/Category.java
+++ trunk/src/entities/Category.java
@@ -1,12 +1,16 @@
 package entities;
 
+import java.util.ArrayList;
+
 public class Category extends BaseEntity {
     private static final long serialVersionUID = -5468875646426363744L;
     private String name;
+    private ArrayList<String> judgeIDs;
 
-    public Category(final String id, final String name) {
+    public Category(final String id, final String name, ArrayList<String> judgeIDs) {
         this.id = id;
         this.name = name;
+        this.judgeIDs = judgeIDs;
     }
     
     public Category() {
@@ -19,5 +23,13 @@ public class Category extends BaseEntity {
 
     public void setName(final String name) {
         this.name = name;
+    }
+    
+    public void setJudgeIDs(final ArrayList<String> judgeIDs) {
+        this.judgeIDs = judgeIDs;
+    }
+    
+    public ArrayList<String> getJudgeIDs() {
+        return this.judgeIDs;
     }
 }
