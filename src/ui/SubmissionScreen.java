af6449e902f9d59d24ae79a736ea99b447a9d1cb
sadak@uw.edu <sadak@uw.edu@96964130-56d7-0125-8f55-f3c4090f176b>
2013-02-16 03:09:00 +0000

    stub Submission screen

---
+++ src/ui/SubmissionScreen.java
@@ -0,0 +1,25 @@
+
+package ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SubmissionScreen extends BaseScreen implements ActionListener {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6685606166954046733L;
+
+    public SubmissionScreen(App application) {
+	super(application);
+	// TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
