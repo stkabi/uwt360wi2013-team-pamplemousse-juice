7dd08e1e7b8071180cef53ecfd83aa4a3ae05b0a
sg1234@uw.edu <sg1234@uw.edu@96964130-56d7-0125-8f55-f3c4090f176b>
2013-02-17 04:15:03 +0000

    this one has the JUnit. Junit is inside the userTest.java. I have only one test method for test.java.

---
+++ trunk/src/entities/UserTest.java
@@ -0,0 +1,49 @@
+package entities;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class UserTest {
+	
+	//create an instance of user and a string
+	static User u = new User();
+    static String pwPlain = "herpderp";
+   
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+        
+        
+        //set password
+        u.setPassword(pwPlain);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetPassword() {
+		assertTrue(u.getPassword().compareTo(pwPlain) != 0);
+	}
+
+	@Test
+	public void testAuthenticate() {
+		assertTrue(u.authenticate(pwPlain));
+	}
+
+}
