8147eb2b600670dc085d46587c355ae0d7a43b91
sadak@uw.edu <sadak@uw.edu@96964130-56d7-0125-8f55-f3c4090f176b>
2013-02-19 23:01:28 +0000

    Don't know why this says it is modified, I havent changed anything here

--- src/ui/LiteHeaderCellRenderer.java
+++ src/ui/LiteHeaderCellRenderer.java
@@ -1,3 +1,4 @@
+
 package ui;
 
 import java.awt.Color;
@@ -11,28 +12,31 @@ import javax.swing.table.TableCellRenderer;
 
 @SuppressWarnings("serial")
 class LiteHeaderCellRenderer extends JLabel implements TableCellRenderer {
-	
-	private Color headerForeColor = new Color(30, 30, 30);
-	private Color headerBackColor = new Color(255, 255, 255);
+
+    private Color headerForeColor = new Color(30, 30, 30);
+    private Color headerBackColor = new Color(255, 255, 255);
     private Color borderColor = new Color(240, 240, 240);
 
- // Implementation of TableCellRenderer interface
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        if (value != null) {
-            setText(value.toString());
-        } else {
-            setText("");
-        }
-        
-        this.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, borderColor), BorderFactory.createEmptyBorder(8, 4, 8, 4)));
-        this.setFont(this.getFont().deriveFont(Font.BOLD));
+    // Implementation of TableCellRenderer interface
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+	    boolean hasFocus, int row, int column) {
+	if (value != null) {
+	    setText(value.toString());
+	} else {
+	    setText("");
+	}
 
-        setSize(table.getColumnModel().getColumn(column).getWidth(), getPreferredSize().height);
-        
-        setOpaque(true);
-        setBackground(headerBackColor);
-        setForeground(headerForeColor);
-        
-        return this;
+	this.setBorder(BorderFactory.createCompoundBorder(
+		BorderFactory.createMatteBorder(0, 0, 1, 0, borderColor),
+		BorderFactory.createEmptyBorder(8, 4, 8, 4)));
+	this.setFont(this.getFont().deriveFont(Font.BOLD));
+
+	setSize(table.getColumnModel().getColumn(column).getWidth(), getPreferredSize().height);
+
+	setOpaque(true);
+	setBackground(headerBackColor);
+	setForeground(headerForeColor);
+
+	return this;
     }
 }
