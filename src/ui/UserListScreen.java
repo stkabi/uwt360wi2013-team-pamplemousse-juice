08945752b54a69a47d806900f5081387cb7510d9
brianmat@uw.edu <brianmat@uw.edu@96964130-56d7-0125-8f55-f3c4090f176b>
2013-02-20 03:21:46 +0000

    added logout to judge and organizer screen

--- src/ui/UserListScreen.java
+++ src/ui/UserListScreen.java
@@ -1,8 +1,15 @@
 package ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 
 import data.DataProvider;
 import entities.User;
@@ -10,6 +17,7 @@ import entities.User;
 public class UserListScreen extends BaseScreen {
     private static final long serialVersionUID = -1373614477586478093L;
     private LiteTable table;
+    private LiteButton logout;
 
     public UserListScreen(App application) {
         super(application);
@@ -17,6 +25,31 @@ public class UserListScreen extends BaseScreen {
         this.setPreferredSize(new Dimension(600, 400));
 
         this.createTable();
+        this.createButtonBar();
+        this.setOpaque(true);
+        this.setBackground(new Color(255,255,255,255));
+    }
+    
+    private void createButtonBar() {
+        logout = new LiteButton("Logout");
+        logout.setBackground(LiteButton.RED);
+        
+        Container buttonContainer = new Container();
+        buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.X_AXIS));
+        buttonContainer.add(logout);
+        buttonContainer.add(new Box.Filler(null, null, null));
+        
+        final App application = this.application;
+        logout.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                application.loginScreen = null;
+                application.showLogin();
+            }
+            
+        });
+        this.add(buttonContainer, BorderLayout.SOUTH);
     }
 
     private void createTable() {
