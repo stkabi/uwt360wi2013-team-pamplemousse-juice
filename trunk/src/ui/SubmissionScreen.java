ece05023b1efad7746e5f3096fbeac2374a588d5
sadak@uw.edu <sadak@uw.edu@96964130-56d7-0125-8f55-f3c4090f176b>
2013-02-25 10:30:26 +0000

    changed field names to match the other team members naming, cleaned up code and added comments.

--- trunk/src/ui/SubmissionScreen.java
+++ trunk/src/ui/SubmissionScreen.java
@@ -1,107 +1,145 @@
+/*
+ * TCSS360 Winter 2013
+ * Team Pamplemouse_Juice
+ * Talal SADAK
+ * Weaving application
+ */
+
 package ui;
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JFileChooser;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JSeparator;
-import javax.swing.JTextArea;
-import javax.swing.SwingConstants;
-import javax.swing.border.Border;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.TitledBorder;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
 
 import data.DataProvider;
 import entities.Entry;
 import entities.User;
 
+/**
+ * Submission screen class.
+ * 
+ * Sets up the submission screen
+ * 
+ * @author Talal Sadak
+ * 
+ */
 public class SubmissionScreen extends BaseScreen implements ActionListener {
 
+	/** Unique serial ID */
 	private static final long serialVersionUID = -7491538055377138917L;
-	private User current_user;
-	private DataProvider user_data;
-	private ArrayList<Entry> entry_list = new ArrayList<Entry>();
 
+	/** the current user of this app */
+	private User u;
+
+	/** the data provider of the application */
+	private DataProvider dp;
+
+	/** the entry list */
+	private ArrayList<Entry> entry_list;
+
+	/** the current user ID */
 	private String userID = "";
+
+	/** the current category */
 	private String categoryID = "";
+
+	/** the weaving pattern description */
 	private String weavingPattern = "";
+
+	/** the fibers in weave description */
 	private String fibersInWeave = "";
+
+	/** the other details string */
 	private String otherDetails = "";
 
+	/** the index of the category to update */
+	private int categoryIndex = 0;
+
+	/** the buttons of this screen */
 	private LiteButton logout, back, submit, user, upload;
+
+	/** the current category title */
 	private LiteTextField cat;
+
+	/** the label holding the picture */
 	private JLabel label;
+
+	/** the buttons array */
 	private LiteButton[] button_arr = { logout, back, submit };
+
+	/** the buttons text array */
 	private String[] button_txt = { "Logout", "Back", "Submit" };
+
+	/** the button colors array */
 	private Color[] button_color = { LiteButton.RED, LiteButton.BLUE,
 			LiteButton.GREEN };
 
+	/** the previous screen */
 	private BaseScreen last_screen;
+
+	/** the image */
 	private BufferedImage my_image;
+
+	/** the text areas holding the description of the entry */
 	private JTextArea details_area, description_area, fibers_in_weave_area;
 
+	/** the path of the image */
+	private String image_path;
+
+	/**
+	 * Constructor of the class.
+	 * 
+	 * @param the_application
+	 *            the application
+	 * @param the_screen
+	 *            the last screen
+	 * @param the_category
+	 *            the categoryID to be added
+	 * @param the_categoryNum
+	 *            the category index from the check box
+	 */
 	public SubmissionScreen(final App the_application,
-			final BaseScreen the_screen, final String the_category) {
+			final BaseScreen the_screen, final String the_category,
+			final int the_categoryNum) {
+
 		super(the_application);
+
+		last_screen = the_screen;
+		u = the_application.getLoggedInUser();
+		dp = the_application.getDataProvider();
+		userID = u.getName();
+		categoryID = the_category;
+		categoryIndex = the_categoryNum;
+		entry_list = dp.getEntriesByUserId(userID);
+
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		this.setBackground(Color.white);
 		this.setBorder(new EmptyBorder(10, 10, 10, 10));
 		this.setPreferredSize(new Dimension(300, 400));
-
-		last_screen = the_screen;
-		current_user = the_application.getLoggedInUser();
-		user_data = the_application.getDataProvider();
-		// user_data = new DataProvider();
-		setupDescription();
-
-		userID = current_user.getName();
-		categoryID = the_category;
-
-		Container buttonContainer = new Container();
-		buttonContainer.setLayout(new BoxLayout(buttonContainer,
-				BoxLayout.X_AXIS));
-
-		Container titleContainer = setupTitleContainer(current_user);
-		setUploadLabel();
-		Container picContainer = setupPicContainer();
-		setupButtons(buttonContainer);
-
 		this.add(Box.createVerticalStrut(10));
-		this.add(titleContainer);
+		this.add(setupTitleContainer());
 		this.add(new JSeparator(JSeparator.HORIZONTAL), -1);
-
 		this.add(setupCategoryContainer());
-		this.add(description_area);
-		this.add(picContainer);
+		this.add(setupDescription());
+		this.add(setupPicContainer());
 		this.add(setupFibersInWeave());
 		this.add(setupOtherDetails());
 		this.add(Box.createVerticalStrut(10));
-		this.add(buttonContainer);
+		this.add(setupButtons());
 	}
 
+	/**
+	 * Sets up the fibers in weave component.
+	 * 
+	 * @return Component the fibers in weave component
+	 */
 	private Component setupFibersInWeave() {
 		fibers_in_weave_area = new JTextArea(3, 1);
 		fibers_in_weave_area.setPreferredSize(fibers_in_weave_area
@@ -143,8 +181,6 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 							JOptionPane.showMessageDialog(null,
 									"You cannot have more than 2 lines",
 									"Message", JOptionPane.INFORMATION_MESSAGE);
-							// fibersInWeave = fibers_in_weave_area.getText();
-							// System.out.print(fibersInWeave);
 							fibers_in_weave_area.setEditable(false);
 
 						}
@@ -153,6 +189,11 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 		return fibers_in_weave_area;
 	}
 
+	/**
+	 * Sets up the category container.
+	 * 
+	 * @return Container the category container
+	 */
 	private Container setupCategoryContainer() {
 		Container category_Container = new Container();
 		category_Container.setLayout(new BoxLayout(category_Container,
@@ -168,7 +209,12 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 		return cat;
 	}
 
-	private void setupDescription() {
+	/**
+	 * Sets up the description component.
+	 * 
+	 * @return Component the description component
+	 */
+	private Component setupDescription() {
 		description_area = new JTextArea(3, 1);
 		description_area.setPreferredSize(description_area.getMinimumSize());
 		description_area.setLineWrap(true);
@@ -209,15 +255,18 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 							JOptionPane.showMessageDialog(null,
 									"You cannot have more than 2 lines",
 									"Message", JOptionPane.INFORMATION_MESSAGE);
-							// weavingPattern = description_area.getText();
-							// System.out.print(description);
 							description_area.setEditable(false);
-
 						}
 					}
 				});
+		return description_area;
 	}
 
+	/**
+	 * Sets up the other details component.
+	 * 
+	 * @return Component the other details component
+	 */
 	private Component setupOtherDetails() {
 		details_area = new JTextArea(3, 1);
 		details_area.setPreferredSize(details_area.getMinimumSize());
@@ -257,25 +306,22 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 					JOptionPane.showMessageDialog(null,
 							"You cannot have more than 2 lines", "Message",
 							JOptionPane.INFORMATION_MESSAGE);
-					// otherDetails = details_area.getText();
-					// System.out.print(otherDetails);
 					details_area.setEditable(false);
-
 				}
 			}
 		});
 		return details_area;
 	}
 
-	private void setUploadLabel() {
-		upload = new LiteButton("Upload...");
-		upload.addActionListener(this);
-		label = new JLabel(new ImageIcon(getClass().getResource(
-				"/res/images/placeholder.png")));
-
-	}
-
-	private void setupButtons(Container buttonContainer) {
+	/**
+	 * Sets up the buttons container.
+	 * 
+	 * @return Container the container of the buttons
+	 */
+	private Container setupButtons() {
+		Container buttonContainer = new Container();
+		buttonContainer.setLayout(new BoxLayout(buttonContainer,
+				BoxLayout.X_AXIS));
 		for (int i = 0; i < button_arr.length; i++) {
 			button_arr[i] = new LiteButton(button_txt[i]);
 			button_arr[i].addActionListener(this);
@@ -290,11 +336,21 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 			}
 		}
 		button_arr[2].setEnabled(false);
+		return buttonContainer;
 	}
 
+	/**
+	 * Sets up the picture container.
+	 * 
+	 * @return Container the container of the picture and upload button
+	 */
 	private Container setupPicContainer() {
 		Container picContainer = new Container();
 		picContainer.setLayout(new BoxLayout(picContainer, BoxLayout.X_AXIS));
+		upload = new LiteButton("Upload...");
+		upload.addActionListener(this);
+		label = new JLabel(new ImageIcon(getClass().getResource(
+				"/res/images/placeholder.png")));
 		picContainer.add(upload);
 		picContainer.add(Box.createRigidArea(new Dimension(80, 30)));
 		picContainer.add(new Box.Filler(null, null, null));
@@ -302,14 +358,19 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 		return picContainer;
 	}
 
-	private Container setupTitleContainer(final User current_user2) {
+	/**
+	 * Sets up the title container.
+	 * 
+	 * @return Container the container of the title and user
+	 */
+	private Container setupTitleContainer() {
 		Container titleContainer = new Container();
 		titleContainer
 				.setLayout(new BoxLayout(titleContainer, BoxLayout.X_AXIS));
 		JLabel title = new JLabel("Submission", null, JLabel.CENTER);
 		titleContainer.add(title);
 		titleContainer.add(Box.createRigidArea(new Dimension(60, 30)));
-		user = new LiteButton("User: " + current_user.getName());
+		user = new LiteButton("User: " + u.getName());
 		user.setBorder(new EmptyBorder(0, 0, 0, 0));
 		user.setForeground(new Color(170, 170, 170));
 		user.setBackground(Color.WHITE);
@@ -324,6 +385,12 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 		return titleContainer;
 	}
 
+	/**
+	 * Handles the event actions for the buttons.
+	 * 
+	 * @param the_event
+	 *            the action event
+	 */
 	@Override
 	public void actionPerformed(final ActionEvent the_event) {
 		final JFileChooser file_chooser = new JFileChooser();
@@ -340,23 +407,17 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 		}
 		// Handle the submit button
 		if (event_object.equals(button_arr[2])) {
-			// TODO: handle Submit using categoryID
-
 			weavingPattern = description_area.getText();
 			otherDetails = details_area.getText();
 			fibersInWeave = fibers_in_weave_area.getText();
 
-			// Entry entry = new Entry(userID, categoryID, false,
-			// weavingPattern,
-			// fibersInWeave, otherDetails);
-			Entry entry = new Entry();
-			entry.setUserID(userID);
-			entry.setCategoryID(categoryID);
-			entry.setWeavingPattern(weavingPattern);
-			entry.setFibersInWeave(fibersInWeave);
-			entry.setWinner(false);
-			entry.setOtherDetails(otherDetails);
-			user_data.saveItem(entry);
+			// Create a new entry with the data from the submission form
+			Entry entry = new Entry(userID, categoryID, false, weavingPattern,
+					fibersInWeave, otherDetails, image_path);
+
+			entry_list.add(categoryIndex, entry);
+
+			dp.saveItem(entry);
 			application.changeScreen(new EntriesScreen(application));
 		}
 		if (event_object.equals(user)) {
@@ -369,10 +430,10 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 	}
 
 	/**
-	 * This method opens the file pointed to by the user.
+	 * Opens the file pointed to by the user.
 	 * 
 	 * @param the_file
-	 *            the file to be open.
+	 *            the file to be opened.
 	 */
 	private void openEvent(final JFileChooser the_file) {
 		final int open_option = the_file.showOpenDialog(this);
@@ -385,7 +446,7 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 				my_image = ImageIO.read(chosen_file);
 				final ImageIcon image_icon = new ImageIcon(
 						my_image.getScaledInstance(64, 64, Image.SCALE_DEFAULT));
-
+				image_path = chosen_file.getAbsolutePath();
 				label.setIcon(image_icon);
 				button_arr[2].setEnabled(true);
 			} catch (final IOException exception) {
@@ -396,6 +457,13 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 		}
 	}
 
+	/**
+	 * Makes sure an image is used.
+	 * 
+	 * @param the_file
+	 *            the file to analyze
+	 * @return Boolean if the file is of the right type
+	 */
 	public Boolean getTypeDescription(final File the_file) {
 		String extension = Utils.getExtension(the_file);
 		Boolean type = false;
@@ -413,6 +481,12 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 		return type;
 	}
 
+	/**
+	 * Inner class to handle the file extensions of the image to upload.
+	 * 
+	 * @author talal Sadak
+	 * 
+	 */
 	public static class Utils {
 		public final static String jpeg = "jpeg";
 		public final static String jpg = "jpg";
@@ -421,10 +495,15 @@ public class SubmissionScreen extends BaseScreen implements ActionListener {
 		public final static String tif = "tif";
 		public final static String png = "png";
 
-		/* Get the extension of a file. */
-		public static String getExtension(File f) {
+		/**
+		 * Get the extension of a file.
+		 * 
+		 * @param the_file
+		 * @return String the file extension
+		 */
+		public static String getExtension(final File the_file) {
 			String ext = null;
-			String s = f.getName();
+			String s = the_file.getName();
 			int i = s.lastIndexOf('.');
 
 			if (i > 0 && i < s.length() - 1) {
