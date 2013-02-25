8cfe5979f23e0d046df1bc98fd1cbb790e42e035
sadak@uw.edu <sadak@uw.edu@96964130-56d7-0125-8f55-f3c4090f176b>
2013-02-25 10:32:28 +0000

    added comments, changed field names to match the other team members

--- src/ui/EntriesScreen.java
+++ src/ui/EntriesScreen.java
@@ -1,60 +1,96 @@
+/*
+ * TCSS360 Winter 2013
+ * Team Pamplemouse_Juice
+ * Talal SADAK
+ * Weaving application
+ */
+
 package ui;
 
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
-import java.util.Enumeration;
-
-import javax.swing.AbstractButton;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.ButtonGroup;
-import javax.swing.JCheckBox;
-import javax.swing.JLabel;
-import javax.swing.JSeparator;
-import javax.swing.border.Border;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.TitledBorder;
+import javax.swing.*;
+import javax.swing.border.*;
 
 import data.DataProvider;
 import entities.Entry;
 import entities.User;
 
+/**
+ * Entries screen class.
+ * 
+ * Sets up the entries screen ui
+ * 
+ * @author Talal Sadak
+ * 
+ */
 public class EntriesScreen extends BaseScreen implements ActionListener {
 
+	/** Unique serial ID */
 	private static final long serialVersionUID = 3747690407033623572L;
-	private User current_user;
-	private DataProvider user_data;
 
-	private ArrayList<Entry> entry_list = new ArrayList<Entry>();
+	/** the current user */
+	private User u;
+
+	/** the data provider */
+	private DataProvider dp;
+
+	/** the entry list */
+	private ArrayList<Entry> entry_list;
+
+	/** the entries text fields */
 	private LiteTextField one, two, three;
+
+	/** the entries text fields array */
 	private LiteTextField[] entries_arr = { one, two, three };
+
+	/** the entries text fields text */
 	private String[] entries_txt = { " Classic ", " Hipster ", " Ancient " };
+
+	/** the buttons of this screen */
 	private LiteButton logout, remove, add, user;
+
+	/** the buttons array */
 	private LiteButton[] button_arr = { logout, remove, add };
+
+	/** the buttons text array */
 	private String[] button_txt = { "Logout", "Remove", "Add" };
+
+	/** the button colors */
 	private Color[] button_color = { LiteButton.RED, LiteButton.BLUE,
 			LiteButton.GREEN };
+
+	/** the check boxes of the categories */
 	private JCheckBox ckbx1, ckbx2, ckbx3;
+
+	/** the check boxes array */
 	private JCheckBox[] ckbx_items = { ckbx1, ckbx2, ckbx3 };
+
+	/** the check boxes text array */
 	private String[] ckbx_txt_arr = { "Classic", "Hipster", "Ancient" };
+
+	/** the check box group container */
 	private ButtonGroup chekBoxGrp = new ButtonGroup();
+
+	/** the initial text entry */
 	private String entryTxt = "Empty";
+
+	/** the category ID text */
 	private String categoryID = "";
 
+	/**
+	 * Constructor for the class.
+	 * 
+	 * @param application
+	 *            the application
+	 */
 	public EntriesScreen(final App application) {
 		super(application);
 
-		current_user = application.getLoggedInUser();
-		user_data = application.getDataProvider();
-		entry_list = user_data.getEntriesByUserId(current_user.getID());
+		u = application.getLoggedInUser();
+		dp = application.getDataProvider();
+		entry_list = dp.getEntriesByUserId(u.getID());
 
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
@@ -67,14 +103,11 @@ public class EntriesScreen extends BaseScreen implements ActionListener {
 				BoxLayout.X_AXIS));
 
 		this.setPreferredSize(new Dimension(300, 400));
-		Border padding = new EmptyBorder(8, 8, 8, 8);
-		padding = new CompoundBorder(BorderFactory.createLineBorder(new Color(
-				200, 200, 200), 1), padding);
 
 		JLabel title = new JLabel("User Entries", null, JLabel.CENTER);
 		titleContainer.add(title);
 		titleContainer.add(Box.createRigidArea(new Dimension(60, 30)));
-		user = new LiteButton("User: " + current_user.getName());
+		user = new LiteButton("User: " + u.getName());
 		user.setBorder(new EmptyBorder(8, 8, 8, 8));
 		user.setForeground(new Color(170, 170, 170));
 		user.setBackground(Color.WHITE);
@@ -114,9 +147,9 @@ public class EntriesScreen extends BaseScreen implements ActionListener {
 			Container entriesContainer = new Container();
 			entriesContainer.setLayout(new BoxLayout(entriesContainer,
 					BoxLayout.LINE_AXIS));
-			// if (entry_list.size() - 1 > i) {
-			// entryTxt = entry_list.get(i).getOtherDetails().toString();
-			// }
+			if (entry_list.size() - 1 > i) {
+				entryTxt = entry_list.get(i).getOtherDetails();
+			}
 			entries_arr[i] = new LiteTextField(entryTxt);
 
 			ckbx_items[i] = new JCheckBox();
@@ -142,30 +175,19 @@ public class EntriesScreen extends BaseScreen implements ActionListener {
 		this.add(buttonContainer);
 	}
 
-	// public int getSelectedCheckBox(ButtonGroup buttonGroup) {
-	// for (Enumeration<AbstractButton> buttons = buttonGroup.getElements();
-	// buttons
-	// .hasMoreElements();) {
-	// JCheckBox button = (JCheckBox) buttons.nextElement();
-	//
-	// if (button.isSelected()) {
-	//
-	// return button.getText();
-	// }
-	// }
-	//
-	// return null;
-	// }
-
+	/**
+	 * Handles the event actions for the buttons.
+	 * 
+	 * @param the_event
+	 *            the action event
+	 */
 	@Override
 	public void actionPerformed(final ActionEvent the_event) {
 		Object event_object = the_event.getSource();
 		int i = 0;
 
-		// get the selected checkbox item
+		// get the selected check box index
 		for (JCheckBox c : ckbx_items) {
-			// JCheckBox cb = (JCheckBox) c;
-
 			if (c.isSelected()) {
 				System.out.println("CheckBox selected: " + i);
 				categoryID = ckbx_txt_arr[i];
@@ -173,8 +195,6 @@ public class EntriesScreen extends BaseScreen implements ActionListener {
 			}
 			i++;
 		}
-
-		// TODO: fix issue with entrylist being empty
 
 		// if (!entry_list.isEmpty()) {
 		// enable the remove button if an entry is present
@@ -192,24 +212,23 @@ public class EntriesScreen extends BaseScreen implements ActionListener {
 		}
 		// Handle the remove button
 		if (event_object.equals(button_arr[1])) {
-			// // TODO: handle removing an entry
-			ArrayList<Entry> entryList = user_data
-					.getEntriesByUserId(current_user.getID());
+			// TODO: fix issue with entry list being empty
+			ArrayList<Entry> entryList = dp
+					.getEntriesByUserId(u.getID());
 
 			Entry entry = entryList.get(i);
 			// entry_list.remove(i);
 			System.out.println("Entry list size: " + entry_list.size());
-			user_data.removeItem(entry);
+			dp.removeItem(entry);
 
 		}
 		// Handle the add button
 		if (event_object.equals(button_arr[2])) {
 			application.changeScreen(new SubmissionScreen(application, this,
-					categoryID));
+					categoryID, i));
 		}
 		// Handle click on the user (editing)
 		if (event_object.equals(user)) {
-
 			application.changeScreen(new EditRegScreen(application, this));
 		}
 
