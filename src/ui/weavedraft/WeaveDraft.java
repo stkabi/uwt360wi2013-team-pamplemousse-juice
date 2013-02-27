439bb44589af329f6144e35c37487302800ba389
brianmat@uw.edu <brianmat@uw.edu@96964130-56d7-0125-8f55-f3c4090f176b>
2013-02-27 03:38:22 +0000

    Made model serializable. Added screenshot ability to weavedraft.

--- src/ui/weavedraft/WeaveDraft.java
+++ src/ui/weavedraft/WeaveDraft.java
@@ -3,7 +3,11 @@ package ui.weavedraft;
 import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
@@ -13,14 +17,14 @@ public class WeaveDraft extends JPanel {
     private Model model;
     private boolean[][] data;
     private GridRenderer warp, tieup, pedals, center;
-    
+
     public void update() {
         for (int i = 0; i < model.gridSize; i += 1) {
             for (int j = 0; j < model.gridSize; j += 1) {
                 data[i][j] = model.getValue(i, j);
             }
         }
-        
+
         warp.invalidate();
         tieup.invalidate();
         pedals.invalidate();
@@ -29,23 +33,51 @@ public class WeaveDraft extends JPanel {
         this.repaint();
     }
 
+    /**
+     * Save a screenshot of the weavedraft to the specified file path
+     * @param path Path to save image to
+     * @return Image file
+     * @throws IOException
+     */
+    public File saveScreenshot(String path) throws IOException {
+        BufferedImage bufImage = new BufferedImage(getSize().width, getSize().height, BufferedImage.TYPE_INT_RGB);
+        paint(bufImage.createGraphics());
+        
+        File imageFile = new File(path);
+        imageFile.createNewFile();
+        ImageIO.write(bufImage, "jpeg", imageFile);
+        
+        return imageFile;
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
     public WeaveDraft(int gridSize, int tieUpSize) {
-        
         model = new Model(gridSize, tieUpSize);
-        
+        init();
+    }
+
+    public WeaveDraft(Model model) {
+        this.model = model;
+        init();
+    }
+
+    private void init() {
         data = new boolean[model.gridSize][model.gridSize];
-        
+
         for (int i = 0; i < model.gridSize; i += 1) {
             for (int j = 0; j < model.gridSize; j += 1) {
                 data[i][j] = model.getValue(i, j);
             }
         }
-        
+
         warp = new GridRenderer(model.tieUpSize - 1, model.gridSize - 1, model.warp);
         tieup = new GridRenderer(model.tieUpSize - 1, model.tieUpSize - 1, model.tieup);
         pedals = new GridRenderer(model.gridSize - 1, model.tieUpSize - 1, model.pedals);
         center = new GridRenderer(model.gridSize - 1, model.gridSize - 1, data);
-        
+
         warp.addMouseListener(new MouseListener() {
             public void mouseClicked(MouseEvent e) { }
             public void mouseEntered(MouseEvent e) { }
@@ -55,56 +87,55 @@ public class WeaveDraft extends JPanel {
                 int cellY = warp.eventToCellY(e);
                 model.toggleWarp(cellX, cellY);
                 update();
-             }
+            }
             public void mouseReleased(MouseEvent e) { }
         });
-        
+
         tieup.addMouseListener(new MouseListener() {
             public void mouseClicked(MouseEvent e) { }
             public void mouseEntered(MouseEvent e) { }
             public void mouseExited(MouseEvent e) { }
-            public void mousePressed(MouseEvent e) { 
+            public void mousePressed(MouseEvent e) {
                 int cellX = tieup.eventToCellX(e);
                 int cellY = tieup.eventToCellY(e);
                 model.toggleTieUp(cellX, cellY);
                 update();
-             }
-            public void mouseReleased(MouseEvent e) {}
+            }
+            public void mouseReleased(MouseEvent e) { }
         });
-        
+
         pedals.addMouseListener(new MouseListener() {
-            public void mouseClicked(MouseEvent e) {}
+            public void mouseClicked(MouseEvent e) { }
             public void mouseEntered(MouseEvent e) { }
             public void mouseExited(MouseEvent e) { }
-            public void mousePressed(MouseEvent e) { 
+            public void mousePressed(MouseEvent e) {
                 int cellX = pedals.eventToCellX(e);
                 int cellY = pedals.eventToCellY(e);
                 model.togglePedals(cellX, cellY);
                 update();
-             }
-            public void mouseReleased(MouseEvent e) {}
+            }
+            public void mouseReleased(MouseEvent e) { }
         });
-        
+
         int width = model.tieUpSize * 20, height = model.gridSize * 20;
         warp.setPreferredSize(new Dimension(height, width));
         tieup.setPreferredSize(new Dimension(width, width));
         pedals.setPreferredSize(new Dimension(width, height));
         center.setPreferredSize(new Dimension(height, height));
-        
+
         JSplitPane left = new JSplitPane(JSplitPane.VERTICAL_SPLIT, warp, center);
         JSplitPane right = new JSplitPane(JSplitPane.VERTICAL_SPLIT, tieup, pedals);
-        
-        
+
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, left, right);
-        
+
         splitPane.setBorder(BorderFactory.createEmptyBorder());
         left.setBorder(BorderFactory.createEmptyBorder());
         right.setBorder(BorderFactory.createEmptyBorder());
-        
+
         splitPane.setEnabled(false);
         left.setEnabled(false);
         right.setEnabled(false);
-        
+
         this.add(splitPane);
     }
 }
