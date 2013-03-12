fd68e50617dd814b240dd6d8f5549a4ee799a43f
sadak@uw.edu <sadak@uw.edu@96964130-56d7-0125-8f55-f3c4090f176b>
2013-03-12 00:20:36 +0000

    changes made by Brian to the judge screen

--- src/ui/JudgeListScreen.java
+++ src/ui/JudgeListScreen.java
@@ -15,59 +15,62 @@ import data.DataProvider;
 import entities.Entry;
 
 public class JudgeListScreen extends BaseScreen {
-    private static final long serialVersionUID = -1373614477586478093L;
-    private LiteTable table;
-    private LiteButton logout;
+	private static final long serialVersionUID = -1373614477586478093L;
+	private LiteTable table;
+	private LiteButton logout;
 
-    public JudgeListScreen(App application) {
-        super(application);
+	public JudgeListScreen(App application) {
+		super(application);
 
-        this.setPreferredSize(new Dimension(300, 400));
+		this.setPreferredSize(new Dimension(300, 400));
 
-        this.createTable();
-        this.createButtonBar();
-        this.setOpaque(true);
-        this.setBackground(new Color(255,255,255,255));
-    }
-    
-    private void createButtonBar() {
-        logout = new LiteButton("Logout");
-        logout.setBackground(LiteButton.RED);
-        
-        Container buttonContainer = new Container();
-        buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.X_AXIS));
-        buttonContainer.add(logout);
-        buttonContainer.add(new Box.Filler(null, null, null));
-        
-        final App application = this.application;
-        logout.addActionListener(new ActionListener() {
+		this.createTable();
+		this.createButtonBar();
+		this.setOpaque(true);
+		this.setBackground(new Color(255, 255, 255, 255));
+	}
 
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                application.loginScreen = null;
-                application.showLogin();
-            }
-            
-        });
-        this.add(buttonContainer, BorderLayout.SOUTH);
-    }
+	private void createButtonBar() {
+		logout = new LiteButton("Logout");
+		logout.setBackground(LiteButton.RED);
 
-    private void createTable() {
-        DataProvider dp = this.application.getDataProvider();
-        ArrayList<Entry> entries = dp.getEntriesForJudge(this.application.getLoggedInUser().getID());
+		Container buttonContainer = new Container();
+		buttonContainer.setLayout(new BoxLayout(buttonContainer,
+				BoxLayout.X_AXIS));
+		buttonContainer.add(logout);
+		buttonContainer.add(new Box.Filler(null, null, null));
 
-        Object[][] data = new Object[entries.size()][3];
+		final App application = this.application;
+		logout.addActionListener(new ActionListener() {
 
-        for (int i = 0; i < entries.size(); i += 1) {
-            data[i][0] = entries.get(i).getID();
-            data[i][1] = dp.getUserById(entries.get(i).getUserID()).getName();
-            data[i][2] = dp.getCategoryById(entries.get(i).getCategoryID()).getName();
-        }
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				application.loginScreen = null;
+				application.showLogin();
+			}
 
-        table = new LiteTable(data, new Object[] { "EntryID", "Name", "Category" });
-        this.setLayout(new BorderLayout());
-        this.add(table.getTableHeader(), BorderLayout.NORTH);
-        this.add(table, BorderLayout.CENTER);
-    }
+		});
+		this.add(buttonContainer, BorderLayout.SOUTH);
+	}
+
+	private void createTable() {
+		DataProvider dp = this.application.getDataProvider();
+		ArrayList<Entry> entries = dp.getEntriesForJudge(this.application
+				.getLoggedInUser().getID());
+
+		Object[][] data = new Object[entries.size()][2];
+
+		for (int i = 0; i < entries.size(); i += 1) {
+
+			data[i][0] = dp.getUserById(entries.get(i).getUserID()).getName();
+			data[i][1] = dp.getCategoryById(entries.get(i).getCategoryID())
+					.getName();
+		}
+
+		table = new LiteTable(data, new Object[] { "Name", "Category" });
+		this.setLayout(new BorderLayout());
+		this.add(table.getTableHeader(), BorderLayout.NORTH);
+		this.add(table, BorderLayout.CENTER);
+	}
 
 }
